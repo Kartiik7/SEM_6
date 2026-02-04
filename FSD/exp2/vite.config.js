71b77f9cab0fc793434078e30e35308bdb3c8b4c
kartiik7 <23bai70520@cuchd.in>
2026-02-04 20:58:56 +0530

    Rebuild dist with correct fsd base path

--- FSD/exp2/vite.config.js
+++ FSD/exp2/vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/SEM_6/exp2_23BAI70520/',
+  base: '/SEM_6/fsd/exp2_23BAI70520/',
 })
